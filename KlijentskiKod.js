8e8b0adc73764d2fb3831833d166efcf838e43df
Jovana Radojko Petrovic <radojkojovana@gmail.com>
2023-03-07 21:32:11 +0100

    Initial commit

---
+++ KlijentskiKod.js
@@ -0,0 +1,17 @@
+function myFunction() {
+    
+var broj = prompt("Unesite broj  od 1 do 5");
+var brojInt = parseInt(broj);
+
+if (brojInt >= 1 && brojInt <= 5){
+    alert("Velika je nesreća kad čovek ne zna šta hoće, a prava katastrofa kad ne zna šta može.");
+}
+
+else if (brojInt < 1 || brojInt > 5){
+    alert("Uneseni broj mora biti izmedju 1 i 5!");
+
+}
+else {
+    alert("Niste uneli broj!");
+}
+}
