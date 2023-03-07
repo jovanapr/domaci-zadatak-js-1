f73446cec87341f2e7ec6a0d9a5cf9fa92d71196
Jovana Radojko Petrovic <radojkojovana@gmail.com>
2023-03-07 21:37:32 +0100

    Fixed indentation

--- KlijentskiKod.js
+++ KlijentskiKod.js
@@ -1,17 +1,17 @@
 function myFunction() {
-    
-var broj = prompt("Unesite broj  od 1 do 5");
-var brojInt = parseInt(broj);
 
-if (brojInt >= 1 && brojInt <= 5){
-    alert("Velika je nesreća kad čovek ne zna šta hoće, a prava katastrofa kad ne zna šta može.");
-}
+    var broj = prompt("Unesite broj  od 1 do 5");
+    var brojInt = parseInt(broj);
 
-else if (brojInt < 1 || brojInt > 5){
-    alert("Uneseni broj mora biti izmedju 1 i 5!");
+    if (brojInt >= 1 && brojInt <= 5) {
+        alert("Velika je nesreća kad čovek ne zna šta hoće, a prava katastrofa kad ne zna šta može.");
+    }
 
-}
-else {
-    alert("Niste uneli broj!");
-}
+    else if (brojInt < 1 || brojInt > 5) {
+        alert("Uneseni broj mora biti izmedju 1 i 5!");
+
+    }
+    else {
+        alert("Niste uneli broj!");
+    }
 }
